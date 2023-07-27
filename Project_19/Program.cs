0c822bc475aa6f94047c7e6e5e6e9c568ce147b0
Ildar <ildar.riki@mail.ru>
2023-07-27 17:47:21 +0400

    Готово

---
+++ Project_19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input <= 10000 && input >= 99999)
+{
+    Console.WriteLine("Некорректный ввод. Пожалуйста, введите пятизначное число.");
+    return;
+}
+
+int num1 = input / 10000;
+int num2 = (input / 1000) % 10;
+int num3 = (input / 10) % 10;
+int num4 = input % 10;
+
+if (num1 == num4 && num2 == num3)
+{
+    Console.WriteLine("Число палиндромное");
+}
+else
+{
+    Console.WriteLine("Число не палиндромное");
+}
---
+++ Project_21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты первой точки (x1, y1, z1):");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки (x2, y2, z2):");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double deltaX = x2 - x1;
+double deltaY = y2 - y1;
+double deltaZ = z2 - z1;
+
+double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+Console.WriteLine($"Distance: {distance}");
