84bb73a9dab2407629755fa1b2803813ed5bd3d7
Ildar <ildar.riki@mail.ru>
2023-08-24 15:01:11 +0400

    41 and 43

---
+++ Project_41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество чисел: ");
+int count = int.Parse(Console.ReadLine());
+
+int positiveCount = 0;
+
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+    {
+        positiveCount++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше 0: {positiveCount}");
