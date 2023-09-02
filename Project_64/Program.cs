647e8a0e7d5063a6a90e0aedfe3f51453384e2a2
Ildar <ildar.riki@mail.ru>
2023-09-02 11:57:52 +0400

    64-68

---
+++ Project_64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Результат: ");
+PrintNumbersFromNto1(N);
+
+Console.ReadLine();
+
+static void PrintNumbersFromNto1(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n);
+        if (n > 1)
+            Console.Write(", ");
+        PrintNumbersFromNto1(n - 1);
+    }
+}
---
+++ Project_66/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+int sum = CalculateSum(M, N);
+
+Console.WriteLine("Сумма натуральных элементов от " + M + " до " + N + " равна: " + sum);
+
+Console.ReadLine();
+
+static int CalculateSum(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
